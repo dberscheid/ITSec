78d6f3ccb9fbbee70861967b612af10d53eff972
tightdelay <tightdelayded@gmail.com>
2018-03-27 19:54:47 +0200

    j

--- CodeFiles/CoriaETL/timer.py
+++ CodeFiles/CoriaETL/timer.py
@@ -2,10 +2,11 @@
 import time, subprocess
 
 while True:
-	Frequency = 60*60*24
-	time.sleep(Frequency)
-	subprocess.call(["python", "main.py"])
-		
+	try:
+		Frequency = 60*60*24
+		time.sleep(Frequency)
+		subprocess.call(["python", "main.py"])
+
 	except:
 		print("Something went wrong. Restarting in"+ str(Frequency/(60*60))+" hours.")
 		continue
